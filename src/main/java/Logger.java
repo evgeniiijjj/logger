ad46e1c317d172b564f4d1c61fae6ed63cd478d6
evgeniiijjj <evgenijjj78@gmail.com>
2022-06-05 02:08:05 +0300

    first commit

---
+++ src/main/java/Logger.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Logger {
+
+    private static Logger logger;
+    private int count;
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if (logger == null) {
+            logger = new Logger();
+        }
+        return logger;
+    }
+
+    public void log(String message) {
+        System.out.printf("\r[%s %s %d] %s\n", LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+                , LocalTime.now().format(DateTimeFormatter.ofPattern("hh.mm.ss")), ++count, message);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+
+    static final Logger logger = Logger.getInstance();
+    static final Scanner scanner = new Scanner(System.in);
+    static final List<Integer> list = new ArrayList<>();
+    static final List<Integer> result = new ArrayList<>();
+    static final Random rnd = new Random();
+
+    public static void main(String[] args) {
+        int step = 0, n = 0, m, f = 0;
+        while (true) {
+            try {
+                switch (step) {
+                    case 0:
+                        logger.log("Запускам программу");
+                        System.out.println("Здравствуйте!");
+                        logger.log("Просим пользователя ввести данные для списка");
+                        step++;
+                    case 1:
+                        System.out.print("Введите размер списка: ");
+                        n = Integer.parseInt(scanner.nextLine());
+                        step++;
+                    case 2:
+                        System.out.print("Введите верхнюю границу для значений: ");
+                        m = Integer.parseInt(scanner.nextLine());
+                        step++;
+                        logger.log("Создаем и наполняем список");
+                        for (int i = 0; i < n; i++) {
+                            list.add(rnd.nextInt(m + 1));
+                        }
+                        System.out.println("Вот случайный список: " + list);
+                        logger.log("Просим пользователя ввести входные данные для фильтрации");
+                    case 3:
+                        System.out.print("Введите порог для фильтра: ");
+                        f = Integer.parseInt(scanner.nextLine());
+                        logger.log("Запускам фильтрацию");
+                        for (int i : list) {
+                            if (i > f) {
+                                result.add(i);
+                                logger.log(String.format("Элемент %d %s", i, "проходит"));
+                            } else {
+                                logger.log(String.format("Элемент %d %s", i, "не проходит"));
+                            }
+                        }
+                        logger.log(String.format("Прошло фильтр %d элементов из %d", result.size(), n));
+                        logger.log("Выводим результат на экран");
+                        System.out.println("Отфильтрованный список: " + result);
+                        logger.log("Завершаем программу");
+                        break;
+                    default:
+                }
+                break;
+            } catch (NumberFormatException e) {
+                logger.log("Данные введены не корректно, просим пользователя ввести данные повторно");
+                System.out.println("Ввод некорректен, повторите ввод");
+            }
+        }
+    }
+}
